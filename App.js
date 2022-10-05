b089bf1cbaf06a41940c2bb0976a904f0f06d784
Zacharias Vlatakis <zachariasvlatakis@gmail.com>
2022-10-05 11:58:03 +0300

    upload

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
 
 export default function App() {
+  const [enterTaskText, setTaskText] = useState("");
+  const [listOfTasks, setListOfTasks] = useState([]);
+
+  function getMyTasks(enteredText) {
+    setTaskText(enteredText);
+  }
+
+  function addMyTask() {
+    setListOfTasks([...listOfTasks, enterTaskText]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='dark' />
+      <Text style={styles.appTitle}>My Todo App</Text>
+      <TextInput
+        style={styles.taskTextInput}
+        placeholder='Write my chore...'
+        placeholderTextColor='#E2E8C0'
+        onChangeText={getMyTasks}></TextInput>
+      <View style={styles.taskButton}>
+        <Button title='Add task' color='#846C5B' onPress={addMyTask}></Button>
+      </View>
+      <View>
+        <View>
+          {listOfTasks.map((task) => (
+            <Text style={styles.taskText}>{task}</Text>
+          ))}
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +40,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 30,
+    backgroundColor: "#443742",
+  },
+  appTitle: {
+    fontWeight: "bold",
+    color: "#E2E8C0",
+    fontSize: 30,
+  },
+  taskTextInput: {
+    color: "#E2E8C0",
+    paddingTop: 5,
+    paddingHorizontal: 20,
+  },
+  taskButton: {
+    paddingHorizontal: 140,
+  },
+  taskText: {
+    color: "#E2E8C0",
+    paddingTop: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#E2E8C0",
   },
 });
